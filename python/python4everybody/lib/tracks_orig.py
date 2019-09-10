102430ff23e6567990245c73caa6f2e0874998b2
ca186022 <christian.arroyo@teradata.com>
2019-09-10 08:48:14 -0700

    9/10

--- python/python4everybody/lib/tracks_orig.py
+++ python/python4everybody/lib/tracks_orig.py
@@ -6,12 +6,10 @@ cur = conn.cursor()
 
 # Make some fresh tables using executescript()
 cur.executescript('''
-
 DROP TABLE IF EXISTS Artist;
 DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS Album;
 DROP TABLE IF EXISTS Track;
-
 
 CREATE TABLE Artist (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -34,7 +32,6 @@ CREATE TABLE Track (
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
-    genre_id  INTEGER,
     len INTEGER, rating INTEGER, count INTEGER
 );
 ''')
@@ -46,7 +43,6 @@ if ( len(fname) < 1 ) : fname = 'Library.xml'
 # <key>Track ID</key><integer>369</integer>
 # <key>Name</key><string>Another One Bites The Dust</string>
 # <key>Artist</key><string>Queen</string>
-# <key>Genre</key><string>Rock</string>
 def lookup(d, key):
     found = False
     for child in d:
@@ -63,16 +59,16 @@ for entry in all:
 
     name = lookup(entry, 'Name')
     artist = lookup(entry, 'Artist')
-    genre = lookup(entry, 'Genre')
     album = lookup(entry, 'Album')
     count = lookup(entry, 'Play Count')
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
+    genre = lookup(entry, 'Genre')
 
-    if name is None or artist is None or album is None or genre is None:
+    if name is None or artist is None or album is None:
         continue
 
-    print(name, artist, album, count, rating, length, genre)
+    print(name, artist, album, count, rating, length)
 
     cur.execute('''INSERT OR IGNORE INTO Artist (name)
         VALUES ( ? )''', ( artist, ) )
@@ -84,14 +80,9 @@ for entry in all:
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
     album_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Genre (name)
-        VALUES ( ? )''', ( genre, ) )
-    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
-    genre_id = cur.fetchone()[0]
-
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count, genre_id)
-        VALUES ( ?, ?, ?, ?, ?, ? )''',
-        ( name, album_id, length, rating, count, genre_id ) )
+        (title, album_id, len, rating, count)
+        VALUES ( ?, ?, ?, ?, ? )''',
+        ( name, album_id, length, rating, count ) )
 
     conn.commit()
