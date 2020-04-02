e76edecc51e0505736158e1601bb5291a3be1857
Christian Arroyo <chris.cap22@gmail.com>
2020-04-02 08:46:52 -0700

    first practice google program

---
+++ python/udemy/google_is_sum_in_array.py
@@ -0,0 +1,23 @@
+def is_sum_in_array(array, sum):
+    i = 0
+    while(i < len(array)):
+        j = 0
+        while(j < len(array)):
+            if array[i] + array[j] == sum:
+                return True
+            j += 1
+        i += 1
+    return False
+
+array = [1, 3, 3, 9]
+sum = 6
+print(is_sum_in_array(array, sum))
+
+# for(i=0; i < array.length(); i++){
+#     for(j=i+1; j < array.length(), j++){
+#         if(array[i] + array[j] == sum){
+#             return true
+#         }
+#     }
+# }
+# return false
