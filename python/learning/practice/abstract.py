7803df4f79292a2a9150baeea4b7421e45a0e36b
christian.arroyo <chris.cap22@gmail.com>
2024-04-23 13:02:05 -0700

    testing

--- python/learning/practice/abstract.py
+++ python/learning/practice/abstract.py
@@ -1,50 +1,58 @@
 from abc import ABC, abstractmethod
 
+
 class Car(ABC):
-    def __init__(self, brand: str, model: str, year: int) -> None:
+    def __init__(self, brand: str, model: str, year: str) -> None:
         self.brand = brand
         self.model = model
         self.year = year
 
     # Abstract method
     @abstractmethod
-    def printDetails(self) -> None:
+    def print_details(self) -> None:
         pass
 
     # Concrete method
-    def accelerate(self) -> None:
+    @staticmethod
+    def accelerate() -> None:
+        """"""
         print("speed up ...")
-    
-    def break_applied(self) -> None:
+
+    @staticmethod
+    def break_applied() -> None:
         print("Car stop")
+
 
 class Hatchback(Car):
 
-    def printDetails(self) -> None:
+    def print_details(self) -> None:
         print("Brand:", self.brand)
         print("Model:", self.model)
         print("Year:", self.year)
-    
-    def sunroof(self) -> None:
+
+    @staticmethod
+    def sunroof() -> None:
         print("Not having this feature")
+
 
 class Suv(Car):
 
-    def printDetails(self) -> None:
+    def print_details(self) -> None:
         print("Brand:", self.brand)
         print("Model:", self.model)
         print("Year:", self.year)
-    
-    def sunroof(self) -> None:
+
+    @staticmethod
+    def sunroof() -> None:
         print("Available")
 
 
-car1 = Hatchback("Maruti", "Alto", "2022"); 
-car1.printDetails() 
+car1 = Hatchback("Maruti", "Alto", "2022")
+car1.print_details()
 car1.accelerate()
-car1.sunroof
+car1.sunroof()
 
-car2 = Suv("Toyota", "RAV4", "2024"); 
-car2.printDetails() 
+car2 = Suv("Toyota", "RAV4", "2024")
+car2.print_details()
 car2.accelerate()
-car2.sunroof
+car2.sunroof()
